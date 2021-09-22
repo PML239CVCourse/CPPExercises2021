b56f83f7a887fa7074a8c4e60833ee2a6fc876b3
QwintenParker <qwintenparker@gmail.com>
2021-09-22 13:01:11 +0300

    lesson03 changed

--- lesson03/src/helper_functions.cpp
+++ lesson03/src/helper_functions.cpp
@@ -1,6 +1,7 @@
 #include "helper_functions.h"
 
 #include <libutils/rasserts.h>
+#include <iostream>
 
 
 cv::Mat makeAllBlackPixelsBlue(cv::Mat image) {
@@ -10,6 +11,7 @@ cv::Mat makeAllBlackPixelsBlue(cv::Mat image) {
     // чем больше значение одного из трех чисел - тем насыщеннее его оттенок
     // всего их диапазон значений - от 0 до 255 включительно
     // т.е. один байт, поэтому мы используем ниже тип unsigned char - целое однобайтовое неотрицательное число
+
     cv::Vec3b color = image.at<cv::Vec3b>(13, 5); // взяли и узнали что за цвет в пикселе в 14-ом ряду (т.к. индексация с нуля) и 6-ой колонке
     unsigned char blue = color[0]; // если это число равно 255 - в пикселе много синего, если равно 0 - в пикселе нет синего
     unsigned char green = color[1];
@@ -19,10 +21,20 @@ cv::Mat makeAllBlackPixelsBlue(cv::Mat image) {
     // поэкспериментируйте! например можете всю картинку заполнить каким-то одним цветом
 
     // пример как заменить цвет по тем же координатам
-    red = 255;
+    //red = 0;
+    //green = 0;
+    //blue = 250;
     // запустите эту версию функции и посмотрите на получившуюся картинку - lesson03/resultsData/01_blue_unicorn.jpg
     // какой пиксель изменился? почему он не чисто красный?
-    image.at<cv::Vec3b>(13, 5) = cv::Vec3b(blue, green, red);
+    for (int i = 0; i < image.cols; ++i) {
+        for (int j = 0; j < image.rows; ++j) {
+            cv::Vec3b color1 = image.at<cv::Vec3b>(j, i);
+            if (color1 == cv::Vec3b(blue, green, red))
+                image.at<cv::Vec3b>(j, i) = cv::Vec3b(255, 0, 0);
+        }
+    }
+
+    //image.at<cv::Vec3b>(13, 5) = cv::Vec3b(blue, green, red);
 
     return image;
 }
@@ -31,6 +43,16 @@ cv::Mat invertImageColors(cv::Mat image) {
     // TODO реализуйте функцию которая каждый цвет картинки инвертирует:
     // т.е. пусть ночь станет днем, а сумрак рассеется
     // иначе говоря замените каждое значение яркости x на (255-x) (т.к находится в диапазоне от 0 до 255)
+    for (int i = 0; i < image.cols; ++i) {
+        for (int j = 0; j < image.rows; ++j) {
+            cv::Vec3b color1 = image.at<cv::Vec3b>(j, i);
+            char blue = color1[0];
+            char green = color1[1];
+            char red = color1[2];
+            image.at<cv::Vec3b>(j, i) = cv::Vec3b(255 - blue, 255 - green, 255 - red);
+        }
+    }
+
 
     return image;
 }
