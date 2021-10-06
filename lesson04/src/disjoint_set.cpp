21ef2048b2751f769eb6e1c11c4d332dbea8df67
TopSerg <mcsergspb@gmail.com>
2021-10-06 12:55:37 +0300

    merge

--- lesson04/src/disjoint_set.cpp
+++ lesson04/src/disjoint_set.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "disjoint_set.h"
 
 #define ROOT -1 // объявили макрос (считайте константу) равный минус единице - чтобы не было "волшебной переменной", а была именованная константа "корень дерева"
@@ -8,6 +9,11 @@ DisjointSet::DisjointSet(int size)
     parents = std::vector<int>(size);
     ranks = std::vector<int>(size);
     sizes = std::vector<int>(size);
+    for (int i = 0; i < size; ++i) {
+        parents[i] = ROOT;
+        ranks[i] = i;
+        sizes[i] = 1;
+    }
     // TODO - заполните вектора так чтобы на этапе конструирования эта система непересекающихся множеств состояла из:
     // size элементов, каждый из которых состоит в своем множестве (а значит ссылка на родителя у него - на корень, т.е. на ROOT, а какой тогда ранк и размер у множества каждого элемента?)
     // TODO заполнить parents
@@ -19,7 +25,21 @@ int	DisjointSet::get_set(int element)
 {
     // TODO по номеру элемента нужно переходя по ссылкам на родителя дойти до самого верхнего элемента,
     // номер этого корневого элемента - номер множества на данный момент (кто вверху тот и главный, множество названо в его честь)
-    int first_of_their_kind = 0;
+    int first_of_their_kind = element;
+    int a = element;
+    while (true){
+        if (a == ROOT){
+            return first_of_their_kind;
+        }
+        else{
+            first_of_their_kind = a;
+            a = parents[first_of_their_kind];
+//            std::cout << a << std::endl;
+        }
+    }
+//    for (int i = 0; i < parents.size(); ++i) {
+//        std::cout << parents[i]<< " ";
+//    }
 
     return first_of_their_kind;
 }
@@ -29,7 +49,9 @@ int DisjointSet::count_differents() const
     // TODO посчитать сколько разных множеств (подсказка: в каждом множестве ровно один корень, а корень - это тот у кого родитель = ROOT)
     int count = 0;
     for (size_t i = 0; i < this->parents.size(); i++) {
-        // ...
+        if (parents[i] == ROOT){
+            count++;
+        }
     }
     return count;
 }
@@ -37,7 +59,7 @@ int DisjointSet::count_differents() const
 int DisjointSet::get_set_size(int element)
 {
     // TODO сообщить сколько элементов в множестве, которому принадлежит данный элемент (да, это очень просто)
-    return 0;
+    return sizes[get_set(element)];
 }
 
 int	DisjointSet::union_sets(int element0, int element1)
@@ -46,6 +68,28 @@ int	DisjointSet::union_sets(int element0, int element1)
     // кого из них подвесить к другому (тем самым объединить два множества)
     // при этом стоит подвешивать менее высокое дерево к более высокому (т.е. учитывая ранк),
     // а так же важно не забыть после переподвешивания у корневого элемента обновить ранк и размер множества
+    if (get_set_size(element0) > get_set_size(element1)){
+        int size = get_set_size(element0) + get_set_size(element1);
+        parents[get_set(element1)] = get_set(element0);
+        sizes[get_set_size(element1)] = size;
+    }
+    else if (get_set_size(element0) < get_set_size(element1)){
+        int size = get_set_size(element0) + get_set_size(element1);
+        parents[get_set(element0)] = get_set(element1);
+        sizes[get_set_size(element1)] = size;
+    }
+    else{
+        if (ranks[get_set(element0)] > ranks[get_set(element1)]){
+            int size = get_set_size(element0) + get_set_size(element1);
+            parents[get_set(element0)] = get_set(element1);
+            sizes[get_set_size(element1)] = size;
+        }
+        else{
+            int size = get_set_size(element0) + get_set_size(element1);
+            parents[get_set(element1)] = get_set(element0);
+            sizes[get_set_size(element1)] = size;
+        }
+    }
 
     return 0; // вернуть номер объединенного множества
 }
