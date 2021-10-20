8294777acf5a54c00eb99561e6306780ac95a289
TushaChesnokova <angarlic@mail.ru>
2021-10-20 11:30:19 +0300

    lesson5 is made

--- lesson05/src/sobel.cpp
+++ lesson05/src/sobel.cpp
@@ -16,15 +16,7 @@ cv::Mat convertBGRToGray(cv::Mat img) {
     for (int j = 0; j < height; ++j) {
         for (int i = 0; i < width; ++i) {
             cv::Vec3b color = img.at<cv::Vec3b>(j, i);
-            unsigned char blue = color[0];
-            unsigned char green = color[1];
-            unsigned char red = color[2];
-
-            // TODO реализуйте усреднение яркости чтобы получить серый цвет
-            //  - обратите внимание что если складывать unsigned char - сумма может переполниться, поэтому перед сложением их стоит преобразовать в int или float
-            //  - загуглите "RGB to grayscale formula" - окажется что правильно это делать не усреднением в равных пропорциях, а с другими коэффициентами
-            float grayIntensity = 0.0f;
-
+            float grayIntensity = (color[0] * 1.0f + color[1] * 1.0f + color[2] * 1.0f) / 3.0f;
             grayscaleImg.at<float>(j, i) = grayIntensity;
         }
     }
@@ -60,33 +52,34 @@ cv::Mat sobelDXY(cv::Mat img) {
             {-1, 0, 1},
     };
 
-    // TODO исправьте коээфициенты свертки по вертикальной оси y
     int dySobelKoef[3][3] = {
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0},
+            {-1, -2, -1},
+            {0,  0,  0},
+            {1,  2,  1},
     };
 
-    // TODO доделайте этот код (в т.ч. производную по оси ty), в нем мы пробегаем по всем пикселям (j,i)
-    for (int j = 0; j < height; ++j) {
-        for (int i = 0; i < width; ++i) {
+
+    for (int j = 1; j < height - 1; ++j) {
+        for (int i = 1; i < width - 1; ++i) {
             float dxSum = 0.0f; // судя будем накапливать производную по оси x
+            float dySum = 0.0f;
 
             // затем пробегаем по окрестности 3x3 вокруг нашего центрального пикселя (j,i)
             for (int dj = -1; dj <= 1; ++dj) {
                 for (int di = -1; di <= 1; ++di) {
                     float intensity = img.at<float>(j + dj, i + di); // берем соседний пиксель из окрестности
-                    dxSum += dxSobelKoef[1 + dj][1 + di] * intensity; // добавляем его яркость в производную с учетом веса из ядра Собеля
+                    dxSum += dxSobelKoef[1 + dj][1 + di] *
+                             intensity; // добавляем его яркость в производную с учетом веса из ядра Собеля
+                    dySum += dySobelKoef[1 + dj][1 + di] * intensity;
                 }
             }
 
-            dxyImg.at<cv::Vec2f>(j, i) = cv::Vec2f(0.0f, 0.0f);
+            dxyImg.at<cv::Vec2f>(j, i) = cv::Vec2f(dxSum, dySum);
         }
     }
 
     return dxyImg; // производная по оси x и оси y (в каждом пикселе два канала - два числа - каждая из компонент производной)
 }
-
 cv::Mat convertDXYToDX(cv::Mat img) {
     rassert(img.type() == CV_32FC2,
             238129037129092); // сверяем что в картинке два канала и в каждом - вещественное число
@@ -106,14 +99,31 @@ cv::Mat convertDXYToDX(cv::Mat img) {
 }
 
 cv::Mat convertDXYToDY(cv::Mat img) {
-    // TODO
-    cv::Mat dyImg;
+    int width = img.cols;
+    int height = img.rows;
+    cv::Mat dyImg(height, width, CV_32FC1); // создаем одноканальную картинку состоящую из 32-битных вещественных чисел
+    for (int j = 0; j < height; ++j) {
+        for (int i = 0; i < width; ++i) {
+            cv::Vec2f dxy = img.at<cv::Vec2f>(j, i);
+
+            float y = std::abs(dxy[1]); // взяли абсолютное значение производной по оси x
+
+            dyImg.at<float>(j, i) = y;
+        }
+    }
     return dyImg;
 }
 
 cv::Mat convertDXYToGradientLength(cv::Mat img) {
-    // TODO реализуйте функцию которая считает силу градиента в каждом пикселе
-    // точнее - его длину, ведь градиент - это вектор (двухмерный, ведь у него две компоненты), а у вектора всегда есть длина - sqrt(x^2+y^2)
-    // TODO и удостоверьтесь что результат выглядит так как вы ожидаете, если нет - спросите меня
-    return img;
+    int width = img.cols;
+    int height = img.rows;
+    cv::Mat res(height, width, CV_32FC1);
+    for (int j = 0; j < height; ++j) {
+        for (int i = 0; i < width; ++i) {
+            cv::Vec2f g = img.at<cv::Vec2f>(j, i);
+
+            res.at<float>(j, i) = std::sqrt(g[0] * g[0] + g[1] * g[1]);
+        }
+    }
+    return res;
 }
