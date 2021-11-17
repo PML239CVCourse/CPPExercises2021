02770df1fe5ced191ed5402311a03be93f405f04
dlinef.22 <eduarddlin@gmail.com>
2021-11-17 10:54:32 +0300

    og win ti 11

--- lesson07/src/main.cpp
+++ lesson07/src/main.cpp
@@ -5,7 +5,7 @@
 #include "hough.h"
 
 #include <opencv2/imgproc.hpp>
-
+using namespace std;
 
 void test(std::string name) {
     std::string full_path = "lesson05/data/" + name + ".jpg";
@@ -25,83 +25,91 @@ void test(std::string name) {
     cv::imwrite("lesson07/resultsData/" + name + "_2_sobel_y.png", grad_y);
 
     // замечаем что мы ведь забыли взять абсолютное значение градиента!
-    // TODO посмотрите на картинки на диске, какая из картинок это явно показывает?
+    //  посмотрите на картинки на диске, какая из картинок это явно показывает?
     // давайте это исправим:
-//    grad_x = cv::abs(grad_x);
-//    grad_y = cv::abs(grad_y);
-//    cv::imwrite("lesson07/resultsData/" + name + "_3_sobel_x.png", grad_x);
-//    cv::imwrite("lesson07/resultsData/" + name + "_4_sobel_y.png", grad_y);
-    // TODO посмотрите на картинки и проверьте поправилось ли?
+    grad_x = cv::abs(grad_x);
+    grad_y = cv::abs(grad_y);
+    cv::imwrite("lesson07/resultsData/" + name + "_3_sobel_x.png", grad_x);
+    cv::imwrite("lesson07/resultsData/" + name + "_4_sobel_y.png", grad_y);
+    //  посмотрите на картинки и проверьте поправилось ли?
 
-//    cv::Mat sobel_strength(img.rows, img.cols, CV_32FC1, 0.0f);
-//    // теперь хочется заполнить sobel_strength силой градиента с учетом обеих осей, т.е. что-то вроде sobel_strength=sqrt(grad_x^2+grad_x^2):
-//    for (int j = 0; j < sobel_strength.rows; ++j) {
-//        for (int i = 0; i < sobel_strength.cols; ++i) {
-//            // TODO реализуйте здесь заполнение sobel_strength с учетом grad_x, grad_y:
-//            float dx = grad_x.at<float>(j, i);
-//            // ...
-//            float gradient_strength = 0.0; // TODO
-//            sobel_strength.at<float>(j, i) = gradient_strength;
-//        }
-//    }
-//    cv::imwrite("lesson07/resultsData/" + name + "_5_sobel_strength.png", sobel_strength);
-//
-//    cv::Mat hough = buildHough(sobel_strength); // TODO теперь зайдите внутрь этой функции и реализуйте построение пространства Хафа
-//
-//    cv::imwrite("lesson07/resultsData/" + name + "_6_hough.png", hough);
-//    // обратите внимание что почти все пространство - яркое белое или черное, это происходит потому что яркость сильно больше чем 255
-//
-//    // TODO поправьте это - найдите максимальную яркость (max_accumulated) среди всей матрицы hough и после этого отнормируйте всю картинку:
-//    // float max_accumulated = 0.0f;
-//    // for () {
-//    //     for () {
-//    //         ...
-//    //     }
-//    // }
-//    // TODO замените каждый пиксель с яркости X на яркость X*255/max_accumulated (т.е. уменьшите диапазон значений):
-//    // for () {
-//    //     for () {
-//    //         ...
-//    //     }
-//    // }
-//    // cv::imwrite("lesson07/resultsData/" + name + "_7_hough_normalized.png", hough*255.0f/max_accumulated);
+    assert(grad_x.rows==img.rows);
+    cv::Mat sobel_strength(grad_x.rows, grad_x.cols, CV_32FC1, 0.0f);
+    // теперь хочется заполнить sobel_strength силой градиента с учетом обеих осей, т.е. что-то вроде sobel_strength=sqrt(grad_x^2+grad_x^2):
+    for (int j = 0; j < sobel_strength.rows; ++j) {
+        for (int i = 0; i < sobel_strength.cols; ++i) {
+            // реализуйте здесь заполнение sobel_strength с учетом grad_x, grad_y:
+            float dx = grad_x.at<float>(j, i);
+            float dy=grad_y.at<float>(j,i);
+            float gradient_strength = sqrt(dx*dx+dy*dy);
+            //cout<<gradient_strength<<endl;
+            assert(gradient_strength<1e10);
+            sobel_strength.at<float>(j, i) = gradient_strength;
+        }
+    }
+    cout<<" hgtef "<<endl;
+    cv::imwrite("lesson07/resultsData/" + name + "_5_sobel_strength.png", sobel_strength);
+    //
+    cv::Mat hough = buildHough(sobel_strength); //  теперь зайдите внутрь этой функции и реализуйте построение пространства Хафа
+    //
+    cv::imwrite("lesson07/resultsData/" + name + "_6_hough.png", hough);
+    //    // обратите внимание что почти все пространство - яркое белое или черное, это происходит потому что яркость сильно больше чем 255
+    //
+    //    // поправьте это - найдите максимальную яркость (max_accumulated) среди всей матрицы hough и после этого отнормируйте всю картинку:
+    cout<<" ok0 "<<endl;
+    float max_accumulated = 0.0f;
+    for (int i=0;i<hough.rows;++i) {
+        for (int j=0;j<hough.cols;++j) {
+            max_accumulated=max(max_accumulated,(float) hough.at<float>(i,j));
+        }
+    }
+    cout<<"ok1 "<<endl;
+    //    //  замените каждый пиксель с яркости X на яркость X*255/max_accumulated (т.е. уменьшите диапазон значений):
+    for (int i=0;i<hough.rows;++i) {
+        for (int j=0;j<hough.cols;++j) {
+            hough.at<float>(i,j)/=max_accumulated;
+            hough.at<float>(i,j)*=(255.0);
+        }
+    }
+    cv::imwrite("lesson07/resultsData/" + name + "_7_hough_normalized.png", hough);
 }
 
 
 int main() {
     try {
-        // TODO посмотрите на результат (аккумулятор-пространство Хафа) на всех этих картинках (раскомментируя их одну за другой)
-        // TODO подумайте и напишите здесь оветы на вопросы:
+        //  посмотрите на результат (аккумулятор-пространство Хафа) на всех этих картинках (раскомментируя их одну за другой)
+        //  подумайте и напишите здесь оветы на вопросы:
         test("line01");
         // 1) Какие координаты примерно должны бы быть у самой яркой точки в картинке line01_7_hough_normalized.png?
-        // ответ:
+        // ответ: (70,30) примерно
 
-//        test("line02");
-//        // 2) Какие координаты примерно должны бы быть у самой яркой точки в картинке line02_7_hough_normalized.png?
-//        // ответ:
-//
-//        test("line11");
-//        // 3) Чем должно бы принципиально отличаться пространство Хафа относительно случая line01?
-//        // ответ:
-//
-//        test("line12");
-//        // 4) Зная правильный ответ из предыдущего случая line11 - как найти правильнйы ответ для line12?
-//        // ответ:
-//
-//        test("line21_water_horizont");
-//        // 5) Сколько должно бы быть ярких точек?
-//        // ответ:
-//
-//        test("multiline1_paper_on_table");
-//        // 6) Сколько должно бы быть ярких точек? Сколько вы насчитали в пространстве Хафа?
-//        // ответ:
-//
-//        test("multiline2_paper_on_table");
-//        // 7) Сколько должно бы быть ярких точек? Сколько вы насчитали в пространстве Хафа? Есть ли интересные наблюдения относительно предыдущего случая?
-//        // ответ:
-//
-//        test("valve");
-//        // 8) Какие-нибудь мысли?
+        test("line02");
+        //        // 2) Какие координаты примерно должны бы быть у самой яркой точки в картинке line02_7_hough_normalized.png?
+        //        // ответ: (5,40)
+        //
+        test("line11");
+        //        // 3) Чем должно бы принципиально отличаться пространство Хафа относительно случая line01?
+        //        // ответ:два скопления
+        //
+        test("line12");
+        //        // 4) Зная правильный ответ из предыдущего случая line11 - как найти правильнйы ответ для line12?
+        //        // ответ:перевести в декартовы координаты,повернуть относительно правой верхней точки,и снова перевести в полярные
+        //
+        test("line21_water_horizont");
+        //        // 5) Сколько должно бы быть ярких точек?
+        //        // ответ:должна бы быть одна,но у меня 3 вроде
+        //
+        test("multiline1_paper_on_table");
+        //        // 6) Сколько должно бы быть ярких точек? Сколько вы насчитали в пространстве Хафа?
+        //        // ответ:должно быть 4,у меня 5
+        //
+        test("multiline2_paper_on_table");
+        //        // 7) Сколько должно бы быть ярких точек? Сколько вы насчитали в пространстве Хафа? Есть ли интересные наблюдения относительно предыдущего случая?
+        //        // ответ:должно быть 4 и у меня 4
+        //
+        test("valve");
+        //        // 8) Какие-нибудь мысли?
+        //        много ярких точек
 
         return 0;
     } catch (const std::exception &e) {
@@ -109,4 +117,3 @@ int main() {
         return 1;
     }
 }
-
