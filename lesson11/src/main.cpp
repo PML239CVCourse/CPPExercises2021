42b1f408e5fa7dfabebda32ec3e8e76675d84417
TopSerg <mcsergspb@gmail.com>
2021-12-08 12:55:34 +0300

    krodetdsa

--- lesson11/src/main.cpp
+++ lesson11/src/main.cpp
@@ -20,17 +20,17 @@ cv::Mat drawContours(int rows, int cols, std::vector<std::vector<cv::Point>> con
     cv::Mat blackImage(rows, cols, CV_8UC3, cv::Scalar(0, 0, 0));
     // теперь мы на ней хотим нарисовать контуры
     cv::Mat imageWithContoursPoints = blackImage.clone();
-//    for (int contourI = 0; contourI < contoursPoints.size(); ++contourI) {
-//        // сейчас мы смотрим на контур номер contourI
-//
-//        cv::Scalar contourColor = randColor(); // выберем для него случайный цвет
-//        std::vector<cv::Point> points = contoursPoints[???]; // TODO 06 вытащите вектор из точек-пикселей соответствующих текущему контуру который мы хотим нарисовать
-//        for (int i = 0; ???) { // TODO 06 пробегите по всем точкам-пикселям этого контура
-//            cv::Point point = ???; // TODO 06 и взяв очередную точку-пиксель - нарисуйте выбранный цвет в этом пикселе картинки:
-//            imageWithContoursPoints.at<cv::Vec3b>(point.y, point.x) = cv::Vec3b(contourColor[0], contourColor[1], contourColor[2]);
-//        }
-//
-//    }
+    for (int contourI = 0; contourI < contoursPoints.size(); ++contourI) {
+        // сейчас мы смотрим на контур номер contourI
+
+        cv::Scalar contourColor = randColor(); // выберем для него случайный цвет
+        std::vector<cv::Point> points = contoursPoints[contourI]; // TODO 06 вытащите вектор из точек-пикселей соответствующих текущему контуру который мы хотим нарисовать
+        for (int i = 0; i < points.size(); i++) { // TODO 06 пробегите по всем точкам-пикселям этого контура
+            cv::Point point = points[i]; // TODO 06 и взяв очередную точку-пиксель - нарисуйте выбранный цвет в этом пикселе картинки:
+            imageWithContoursPoints.at<cv::Vec3b>(point.y, point.x) = cv::Vec3b(contourColor[0], contourColor[1], contourColor[2]);
+        }
+
+    }
 
     return imageWithContoursPoints;
 }
@@ -59,61 +59,79 @@ void test(std::string name, std::string k) {
     cv::cvtColor(original, img, cv::COLOR_BGR2GRAY);
     cv::imwrite(out_path + "/01_grey.jpg", img);
 
+    std::cout << "Processing 01 ..." << std::endl;
     // TODO 01 выполните бинарный трешолдинг картинки, прочитайте документацию по функции cv::threshold и выберите значения аргументов
-//    cv::Mat binary;
-//    cv::threshold(img, binary, ???, ???, cv::THRESH_BINARY);
-//    cv::imwrite(out_path + "/02_binary_thresholding.jpg", binary);
+    cv::Mat binary;
+    cv::threshold(img, binary, 127, 255, cv::THRESH_BINARY);
+    cv::imwrite(out_path + "/02_binary_thresholding.jpg", binary);
 
+    std::cout << "Processing 02 ..." << std::endl;
     // TODO 02 выполните адаптивный бинарный трешолдинг картинки, прочитайте документацию по cv::adaptiveThreshold
-//    cv::adaptiveThreshold(img, binary, 255, ???, cv::THRESH_BINARY, ???, ???);
-//    cv::imwrite(out_path + "/03_adaptive_thresholding.jpg", binary);
+    cv::adaptiveThreshold(img, binary, 255, cv::ADAPTIVE_THRESH_MEAN_C , cv::THRESH_BINARY, 5, 15);
+    cv::imwrite(out_path + "/03_adaptive_thresholding.jpg", binary);
 
+    std::cout << "Processing 03e ..." << std::endl;
     // TODO 03 чтобы буквы не разваливались на кусочки - морфологическое расширение (эрозия)
-//    cv::Mat binary_eroded;
-//    cv::erode(binary, binary_eroded, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(4, 4)));
-//    cv::imwrite(out_path + "/04_erode.jpg", binary_eroded);
+    cv::Mat binary_eroded;
+    cv::erode(binary, binary_eroded, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(4, 4)));
+    cv::imwrite(out_path + "/04_erode.jpg", binary_eroded);
 
+    std::cout << "Processing 03d ..." << std::endl;
     // TODO 03 заодно давайте посмотрим что делает морфологическое сужение (диляция)
-//    cv::Mat binary_dilated;
-//    cv::dilate(binary, binary_dilated, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(4, 4)));
-//    cv::imwrite(out_path + "/05_dilate.jpg", binary_dilated);
+    cv::Mat binary_dilated;
+    cv::dilate(binary, binary_dilated, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(4, 4)));
+    cv::imwrite(out_path + "/05_dilate.jpg", binary_dilated);
 
+    std::cout << "Processing 04 ..." << std::endl;
     // TODO 04 дальше работаем с картинкой после морфологичесокго рашсирения или морфологического сжатия - на ваш выбор, подумайте и посмотрите на картинки
-//    binary = binary_dilated/binary_eroded;
+    binary = binary_eroded;
 
     // TODO 05
-//    std::vector<std::vector<cv::Point>> contoursPoints; // по сути это вектор, где каждый элемент - это одна связная компонента-контур,
-//                                                        // а что такое компонента-контур? это вектор из точек (из пикселей)
-//    cv::findContours(binary, contoursPoints, ???, ???); // TODO подумайте, какие нужны два последних параметра? прочитайте документацию, после реализации отрисовки контура - поиграйте с этими параметрами чтобы посмотреть как меняется результат
-//    std::cout << "Contours: " << contoursPoints.size() << std::endl;
-//    cv::Mat imageWithContoursPoints = drawContours(img.rows, img.cols, contoursPoints); // TODO 06 реализуйте функцию которая покажет вам как выглядят найденные контура
-//    cv::imwrite(out_path + "/06_contours_points.jpg", imageWithContoursPoints);
+    std::vector<std::vector<cv::Point>> contoursPoints; // по сути это вектор, где каждый элемент - это одна связная компонента-контур,
+                                                        // а что такое компонента-контур? это вектор из точек (из пикселей)
+    cv::findContours(binary, contoursPoints, cv::RETR_LIST, cv::RETR_LIST); // TODO подумайте, какие нужны два последних параметра? прочитайте документацию, после реализации отрисовки контура - поиграйте с этими параметрами чтобы посмотреть как меняется результат
+    std::cout << "Contours: " << contoursPoints.size() << std::endl;
+    cv::Mat imageWithContoursPoints = drawContours(img.rows, img.cols, contoursPoints); // TODO 06 реализуйте функцию которая покажет вам как выглядят найденные контура
+    std::cout << "Saving" << std::endl;
+    cv::imwrite(out_path + "/06_contours_points.jpg", imageWithContoursPoints);
 
-//    std::vector<std::vector<cv::Point>> contoursPoints2;
-//    cv::findContours(binary, contoursPoints2, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
-//    // TODO:
-//    // Обратите внимание на кромку картинки - она всё победила, т.к. черное - это ноль - пустота, а белое - это 255 - сам объект интереса
-//    // как перевернуть ситуацию чтобы периметр не был засчитан как контур?
-//    // когда подумаете - замрите! и прежде чем кодить:
-//    // Посмотрите в документации у функций cv::threshold и cv::adaptiveThreshold
-//    // про некоего cv::THRESH_BINARY_INV, чем он отличается от cv::THRESH_BINARY?
-//    // Посмотрите как изменились все картинки.
-//    std::cout << "Contours2: " << contoursPoints2.size() << std::endl;
-//    cv::Mat imageWithContoursPoints2 = drawContours(img.rows, img.cols, contoursPoints2);
-//    cv::imwrite(out_path + "/07_contours_points2.jpg", imageWithContoursPoints2);
+    std::vector<std::vector<cv::Point>> contoursPoints2;
+    cv::findContours(binary, contoursPoints2, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    // TODO:
+    // Обратите внимание на кромку картинки - она всё победила, т.к. черное - это ноль - пустота, а белое - это 255 - сам объект интереса
+    // как перевернуть ситуацию чтобы периметр не был засчитан как контур?
+    // когда подумаете - замрите! и прежде чем кодить:
+    // Посмотрите в документации у функций cv::threshold и cv::adaptiveThreshold
+    // про некоего cv::THRESH_BINARY_INV, чем он отличается от cv::THRESH_BINARY?
+    // Посмотрите как изменились все картинки.
+    std::cout << "Contours2: " << contoursPoints2.size() << std::endl;
+    cv::Mat imageWithContoursPoints2 = drawContours(img.rows, img.cols, contoursPoints2);
+    cv::threshold(imageWithContoursPoints2.clone(), imageWithContoursPoints2, 127, 255, cv::THRESH_BINARY_INV);
+    std::cout << "Saving" << std::endl;
+    cv::imwrite(out_path + "/07_contours_points2.jpg", imageWithContoursPoints2);
+    for (int i = 0; i < imageWithContoursPoints2.cols; ++i) {
+        for (int j = 0; j < imageWithContoursPoints2.rows; ++j) {
+            if(imageWithContoursPoints2.at<cv::Vec3b>(j,i)[0] == 0){
+                imageWithContoursPoints.at<cv::Vec3b>(j,i) = imageWithContoursPoints2.at<cv::Vec3b>(j,i);
+            }
+        }
+    }
+    std::cout << "Saving" << std::endl;
+    cv::imwrite(out_path + "/071_contours_points.jpg", imageWithContoursPoints);
 
     // TODO 06 наконец давайте посмотрим какие буковки нашлись - обрамим их прямоугольниками
-//    cv::Mat imgWithBoxes = original.clone();
-//    for (int contourI = 0; contourI < contoursPoints.size(); ++contourI) {
-//        std::vector<cv::Point> points = contoursPoints[contourI]; // перем очередной контур
-//        cv::Rect box = cv::boundingRect(points); // строим прямоугольник по всем пикселям контура (bounding box = бокс ограничивающий объект)
-//        cv::Scalar blackColor(0, 0, 0);
-//        // TODO прочитайте документацию cv::rectangle чтобы нарисовать прямоугольник box с толщиной 2 и черным цветом (обратите внимание какие есть поля у box)
-//        cv::rectangle(imgWithBoxes, ???, ???, ???, ???);
-//    }
-//    cv::imwrite(out_path + "/08_boxes.jpg", imgWithBoxes); // TODO если вдруг у вас в картинке странный результат
-//                                                           // например если нет прямоугольников - посмотрите в верхний левый пиксель - белый ли он?
-//                                                           // если не белый, то что это значит? почему так? сколько в целом нашлось связных компонент?
+    cv::Mat imgWithBoxes = original.clone();
+    for (int contourI = 0; contourI < contoursPoints.size(); ++contourI) {
+        std::vector<cv::Point> points = contoursPoints[contourI]; // берем очередной контур
+        cv::Rect box = cv::boundingRect(points); // строим прямоугольник по всем пикселям контура (bounding box = бокс ограничивающий объект)
+        cv::Scalar blackColor(0, 0, 0);
+        // TODO прочитайте документацию cv::rectangle чтобы нарисовать прямоугольник box с толщиной 2 и черным цветом (обратите внимание какие есть поля у box)
+        cv::rectangle(imgWithBoxes, box, blackColor, 1, cv::LINE_8, 0);
+    }
+    std::cout << "Saving" << std::endl;
+    cv::imwrite(out_path + "/08_boxes.jpg", imgWithBoxes); // TODO если вдруг у вас в картинке странный результат
+                                                           // например если нет прямоугольников - посмотрите в верхний левый пиксель - белый ли он?
+                                                           // если не белый, то что это значит? почему так? сколько в целом нашлось связных компонент?
 }
 
 void finalExperiment(std::string name, std::string k) {
