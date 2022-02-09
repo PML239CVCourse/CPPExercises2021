4b0cb248e826b7882f4eece65f54fcc069b39089
dm1trysh <dmitrysh208@gmail.com>
2022-02-09 11:58:08 +0300

    task3 complited

--- lesson13/src/main.cpp
+++ lesson13/src/main.cpp
@@ -544,6 +544,154 @@ void test3() {
     std::string caseName = "3_hanging2";
 
     // TODO то же самое
+    std::string path = "lesson13/data/" + caseName + "/";
+    std::string results = "lesson13/resultsData/" + caseName + "/";
+
+    if (!std::filesystem::exists(results)) { // если папка для результатов еще не создана
+        std::filesystem::create_directory(results); // то создаем ее
+    }
+
+    cv::Mat img0 = cv::imread(path + "hanging0.png");
+    cv::Mat img1 = cv::imread(path + "hanging1.png");
+
+    rassert(!img0.empty() && !img1.empty(), 2389827851080019);
+    rassert(img0.type() == CV_8UC3, 2389827851080020);
+    rassert(img1.type() == CV_8UC3, 2389827851080021);
+
+    // Этот объект - алгоритм SIFT (детектирования и описания ключевых точек)
+    cv::Ptr<cv::FeatureDetector> detector = cv::SIFT::create();
+
+    // Детектируем и описываем ключевые точки
+    std::vector<cv::KeyPoint> keypoints0, keypoints1; // здесь будет храниться список ключевых точек
+    cv::Mat descriptors0, descriptors1; // здесь будут зраниться дескрипторы этих ключевых точек
+    std::cout << "Detecting SIFT keypoints and describing them (computing their descriptors)..." << std::endl;
+    detector->detectAndCompute(img0, cv::noArray(), keypoints0, descriptors0);
+    detector->detectAndCompute(img1, cv::noArray(), keypoints1, descriptors1);
+    std::cout << "SIFT keypoints detected and described: " << keypoints0.size() << " and " << keypoints1.size() << std::endl; // TODO
+
+
+    {
+        cv::Mat img0withKeypoints, img1withKeypoints;
+        cv::drawKeypoints(img0, keypoints0, img0withKeypoints);
+        cv::drawKeypoints(img1, keypoints1, img1withKeypoints);
+        cv::imwrite(results + "01keypoints0.jpg", img0withKeypoints);
+        cv::imwrite(results + "01keypoints1.jpg", img1withKeypoints);
+    }
+
+
+    // Теперь давайте сопоставим ключевые точки между картинкой 0 и картинкой 1:
+    // найдя для каждой точки из первой картинки - ДВЕ самые похожие точки из второй картинки
+    std::vector<std::vector<cv::DMatch>> matches01;
+    matching0_1(matches01, keypoints0, keypoints1, descriptors0, descriptors1, img0, img1,results);
+
+    // Теперь давайте сопоставим ключевые точки между картинкой 1 и картинкой 0 (т.е. в обратную сторону):
+    std::vector<std::vector<cv::DMatch>> matches10;
+    matching1_0(matches10, keypoints0, keypoints1, descriptors0, descriptors1, img0, img1,results);
+
+
+
+    // Теперь давайте попробуем убрать ошибочные сопоставления
+    std::cout << "Filtering matches..." << std::endl;
+    std::vector<cv::Point2f> points0, points1; // здесь сохраним координаты ключевых точек для удобства позже
+    std::vector<unsigned char> matchIsGood; // здесь мы отметим true - хорошие сопоставления, и false - плохие
+    int nmatchesGood = 0; // посчитаем сколько сопоставлений посчиталось хорошими
+    for (int i = 0; i < keypoints0.size(); ++i) {
+        cv::DMatch match = matches01[i][0];
+        rassert(match.queryIdx == i, 234782749278097); // и вновь - queryIdx это откуда точки (поэтому всегда == i)
+        int j = match.trainIdx; // и trainIdx - это какая точка из второго массива точек оказалась к нам (к queryIdx из первого массива точек) ближайшей
+        rassert(j < keypoints1.size(), 38472957238099); // поэтому явно проверяем что индекс не вышел за пределы второго массива точек
+
+        points0.push_back(keypoints0[i].pt);
+        points1.push_back(keypoints1[j].pt);
+
+        bool isOk = true;
+
+        isOk = isOk && k_ratio_test(i, matches01);
+
+        isOk = isOk && left_right_check(i, matches01, matches10);
+
+        if (isOk) {
+            ++nmatchesGood;
+            matchIsGood.push_back(true);
+        } else {
+            matchIsGood.push_back(false);
+        }
+    }
+    rassert(points0.size() == points1.size(), 3497282579850108);
+    rassert(points0.size() == matchIsGood.size(), 3497282579850109);
+    std::cout << nmatchesGood << "/" << matchIsGood.size() << " good matches left" << std::endl;
+
+    {
+        std::vector<cv::DMatch> goodMatches;
+        for (int i = 0; i < matches01.size(); ++i) {
+            cv::DMatch match = matches01[i][0];
+            rassert(match.queryIdx == i, 2347982739280182);
+            //rassert(match.trainIdx < points1.size(), 2347982739280182);
+            rassert(match.trainIdx < keypoints1.size(), 2347982739280182);
+            if (!matchIsGood[i])
+                continue;
+
+            goodMatches.push_back(match);
+        }
+        cv::Mat imgWithMatches;
+        // визуализируем хорошие соопставления
+        cv::drawMatches(img0, keypoints0, img1, keypoints1, goodMatches, imgWithMatches);
+        cv::imwrite(results + "04goodMatches01.jpg", imgWithMatches);
+    }
+
+    // Теперь на базе оставшихся хороших сопоставлений - воспользуемся готовым методом RANSAC в OpenCV чтобы найти матрицу преобразования из первой картинки во вторую
+    std::cout << "Finding Homography matrix from image0 to image1..." << std::endl;
+    const double ransacReprojThreshold = 3.0;
+    std::vector<cv::Point2f> pointsGood0 = filterPoints(points0, matchIsGood);
+    std::vector<cv::Point2f> pointsGood1 = filterPoints(points1, matchIsGood);
+    std::vector<unsigned char> inliersMask; // в этот вектор RANSAC запишет флажки - какие сопоставления он посчитал корректными (inliers)
+    // RANSAC:
+    cv::Mat H01 = cv::findHomography(pointsGood0, pointsGood1, cv::RANSAC, ransacReprojThreshold, inliersMask);
+    // H01 - матрица 3х3 описывающая преобразование плоскости первой картинки в плоскость второй картинки
+    std::cout << "homography matrix found:" << std::endl;
+    std::cout << H01 << std::endl;
+    {
+        std::vector<cv::DMatch> inliersMatches;
+        std::vector<cv::KeyPoint> inlierKeypoints0, inlierKeypoints1;
+
+        int ninliers = 0;
+        for (int i = 0; i < inliersMask.size(); ++i) {
+            if (inliersMask[i]) {
+                ++ninliers;
+                cv::DMatch match;
+                match.queryIdx = inlierKeypoints0.size();
+                match.trainIdx = inlierKeypoints1.size();
+                inliersMatches.push_back(match);
+                inlierKeypoints0.push_back(cv::KeyPoint(pointsGood0[i], 3.0));
+                inlierKeypoints1.push_back(cv::KeyPoint(pointsGood1[i], 3.0));
+            }
+        }
+
+        // визуализируем inliersMask - т.е. какие сопоставления в конечном счете RANSAC посчитал корректными (т.е. не выбросами, не outliers)
+        std::cout << "(with " << ninliers << "/" << nmatchesGood << " inliers matches)" << std::endl;
+        cv::Mat imgWithInliersMatches;
+        cv::drawMatches(img0, inlierKeypoints0, img1, inlierKeypoints1, inliersMatches, imgWithInliersMatches);
+        cv::imwrite(results + "05inliersMatches01.jpg", imgWithInliersMatches);
+    }
+
+    cv::Mat H10 = H01.inv();
+
+    //cv::Mat img0to1 = img1.clone();
+    //cv::warpPerspective(img0, img0to1, H01, img1.size() + img0.size());
+    //cv::imwrite(results + "hiking_final.jpg", img0to1);
+
+    cv::Mat img1to0(img0.rows*2, img0.cols*2, CV_8UC3, cv::Scalar(0, 0, 0));
+    cv::imwrite(results + "hanging_not_final.jpg", img1to0);
+
+    cv::warpPerspective(img1, img1to0, H10, img1to0.size(),  cv::INTER_LINEAR, cv::BORDER_TRANSPARENT);
+    cv::imwrite(results + "hanging_final.jpg", img1to0);
+    for(int y = 0; y < img0.rows; y++){
+        for(int x = 0; x < img0.cols; x++){
+            cv::Vec3b color = img0.at<cv::Vec3b>(cv::Point(x,y));
+            img1to0.at<cv::Vec3b>(cv::Point(x, y)) = color;
+        }
+    }
+    cv::imwrite(results + "hiking_final_new.jpg", img1to0);
 }
 
 void test4() {
@@ -557,8 +705,8 @@ void test4() {
 int main() {
     try {
         //test1(); // TODO обязательное
-        test2(); // TODO обязательное
-//        test3(); // TODO обязательное
+        //test2(); // TODO обязательное
+        test3(); // TODO обязательное
 
 //        test4(); // TODO добровольный бонус
 
