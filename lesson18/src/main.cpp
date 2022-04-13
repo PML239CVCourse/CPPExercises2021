83882e81f6ee1f0026cdb9aacf90e39c3a644da0
TopSerg <mcsergspb@gmail.com>
2022-04-13 12:56:40 +0300

    umer

--- lesson18/src/main.cpp
+++ lesson18/src/main.cpp
@@ -20,19 +20,24 @@ bool isPixelMasked(cv::Mat mask, int j, int i) {
     rassert(i >= 0 && i < mask.cols, 372489347280018);
     rassert(mask.type() == CV_8UC3, 2348732984792380019);
 
-    if (mask.at<cv::Vec3b>(j,i)[0] + mask.at<cv::Vec3b>(j,i)[1] + mask.at<cv::Vec3b>(j,i)[2] >= 254*3){
+    if (mask.at<cv::Vec3b>(j,i)[0] == 255 && mask.at<cv::Vec3b>(j,i)[1] == 255 && mask.at<cv::Vec3b>(j,i)[2] == 255){
         return true;
     }
     // TODO проверьте белый ли пиксель
     return false;
 }
 
-double estimateQuality(cv::Mat image, int j, int i, int nx, int ny, int w, int h){
+double estimateQuality(cv::Mat image, int j, const int i, int nx, const int ny, int w, int h){
     int w1 = -2, w2 = 2, h1 = -2, h2 = 2, sum1 = 0, sum2 = 0;
     for (int k = w1; k <= w2; ++k) {
         for (int q = h1; q <= h2; ++q) {
-            sum2 += image.at<cv::Vec3b>(k+nx,q+ny)[0]*image.at<cv::Vec3b>(k+nx,q+ny)[0]+image.at<cv::Vec3b>(k+nx,q+ny)[1]*image.at<cv::Vec3b>(k+nx,q+ny)[1]+image.at<cv::Vec3b>(k+nx,q+ny)[2]*image.at<cv::Vec3b>(k+nx,q+ny)[2];
-            sum1 += image.at<cv::Vec3b>(k+j,q+i)[0]*image.at<cv::Vec3b>(k+j,q+i)[0]+image.at<cv::Vec3b>(k+j,q+i)[1]*image.at<cv::Vec3b>(k+j,q+i)[1]+image.at<cv::Vec3b>(k+j,q+i)[2]*image.at<cv::Vec3b>(k+j,q+i)[2];
+            int a = k+nx, b = q+ny, c = k+i, d = q+j;
+            if (a >= 0 && b >= 0 && c >= 0 && d >= 0 && a < image.cols && b < image.rows && c < image.cols && d < image.rows){
+                cv::Vec3b color2 = image.at<cv::Vec3b>(b,a);
+                cv::Vec3b color1 =  image.at<cv::Vec3b>(d,c);
+                sum2 += color2[0]*color2[0]+color2[1]*color2[1]+color2[2]*color2[2];
+                sum1 += color1[0]*color1[0]+color1[1]*color1[1]+color1[2]*color1[2];
+            }
         }
     }
     return sqrt(abs(sum2-sum1));
@@ -89,23 +94,51 @@ void run(int caseNumber, std::string caseName) {
     //
     // Ориентировочный псевдокод-подсказка получившегося алгоритма:
     cv::Mat shifts(original.rows, original.cols, CV_32SC2, cv::Scalar(0, 0)); // матрица хранящая смещения, изначально заполнена парами нулей
-     cv::Mat image = original; // текущая картинка
+     cv::Mat image = original.clone(); // текущая картинка
+     for (int i = 0; i < original.cols; i++) {
+         for (int j = 0; j < original.rows; ++j) {
+             if (mask.at<cv::Vec3b>(j,i)[0] == 255){
+                 image.at<cv::Vec3b>(j,i) = cv::Vec3b(255,255,255);
+             }
+         }
+     }
+     std::cout << original.cols << " " << original.rows << std::endl;
      for (int q = 0; q < 100; q++) {
-         for (int i = 0; i < original.rows; i++) {
-             for (int j = 0; j < original.cols; ++j) {
-                 if (isPixelMasked(mask,j,i)){
+//         std::cout << q << std::endl;
+         for (int i = 0; i < original.cols; i++) {
+             for (int j = 0; j < original.rows; ++j) {
+//                 std::cout << i << " " << j << std::endl;
+//                if (j == 650 && q == 53){
+//                    std::cout << i << std::endl;
+//                }
+                 if (!isPixelMasked(mask,j,i)){
                      continue;
                  }
-                 cv::Vec2i dxy = shifts.at<cv::Vec2b>(j,i);
+//                 if (j == 650 && q == 53){
+//                     std::cout << 348756032 << std::endl;
+//                 }
+//                 if (i == 194 && j == 309){
+//                     std::cout << 1243 << std::endl;
+//                 }
+                 cv::Vec2i dxy = shifts.at<cv::Vec2i>(j,i);
                  int nx = i + dxy[0], ny = j + dxy[1];
-                 double currentQuality = estimateQuality(image, j, i, ny, nx, 5, 5);
-                 int rx = random.next(-i+4, -i+original.rows-4);
-                 int ry = random.next(-j+4, -j+original.cols-4);
-                 double randomQuality = estimateQuality(image, j, i, j+ry, i+rx, 5, 5);
+                 double currentQuality = estimateQuality(image, j, i, nx, ny, 5, 5);
+                 int rx = random.next(-i+4, -i+original.cols-4);
+                 int ry = random.next(-j+4, -j+original.rows-4);
+                 rassert(i+rx >= 0, 876523987)
+                 rassert(i+rx < original.cols, 876523987)
+                 rassert(j+ry >= 0, 876523987)
+                 rassert(j+ry <  original.rows, 876523987)
+                 double randomQuality = estimateQuality(image, j, i,  i+rx, j+ry , 5, 5);
+//                 if (i == 194 && j == 309){
+//                     std::cout << currentQuality << " " << randomQuality << std::endl;
+//                 }
                  if (randomQuality < currentQuality){
-                     shifts.at<cv::Vec2b>(j,i) = cv::Vec2i(rx, ry);
+                     shifts.at<cv::Vec2i>(j,i) = cv::Vec2i(rx, ry);
                      image.at<cv::Vec3b>(j,i) = image.at<cv::Vec3b>(j+ry, i+rx);
+                     currentQuality = randomQuality;
                  }
+
              }
          }
     //         if (если этот пиксель не отмаскирован)
@@ -130,16 +163,17 @@ void run(int caseNumber, std::string caseName) {
     //     не забываем сохранить на диск текущую картинку
     //     а как численно оценить насколько уже хорошую картинку мы смогли построить? выведите в консоль это число
      }
+     cv::imwrite(resultsDir + "10final.png", image);
 }
 
 
 int main() {
     try {
-        run(1, "mic");
+//        run(1, "mic");
         // TODO протестируйте остальные случаи:
 //        run(2, "flowers");
 //        run(3, "baloons");
-//        run(4, "brickwall");
+        run(4, "brickwall");
 //        run(5, "old_photo");
 //        run(6, "your_data"); // TODO придумайте свой случай для тестирования (рекомендуется не очень большое разрешение, например 300х300)
 
